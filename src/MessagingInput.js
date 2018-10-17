1c81dea96ea24d29cf9a646dcb0f87f34090a30f
github username <wireless.patrick@gmail.com>
2018-10-17 10:55:36 -0400

    enabled keydown enter to send message to socket server

--- src/MessagingInput.js
+++ src/MessagingInput.js
@@ -5,15 +5,24 @@ class MessagingInput extends Component {
   send = (message) => this.props.connection.send(JSON.stringify(message))
 
   handleMessaging = (e) => {
-    e.preventDefault()
-    this.send({username: e.target.username.value, message: e.target.message.value})
-    e.target.message.value = ""
+    if ( e.type === 'Submit' ) {
+      e.preventDefault()
+      this.send({username: e.target.username.value, message: e.target.message.value})
+      e.target.message.value = ""
+    } else if (e.type === 'keydown' && e.key === 'Enter') {
+      e.preventDefault()
+      this.send({username: e.currentTarget.username.value, message: e.currentTarget.message.value})
+      e.currentTarget.message.value = ""
+    }
+  }
+
+  componentDidMount() {
   }
 
   render() {
     return (
       <div id="MessageInput">
-        <form onSubmit={this.handleMessaging}><br/>
+        <form onSubmit={this.handleMessaging} onKeyDown={this.handleMessaging}><br/>
           <input type="text" id="username"/><br/>
           <textarea id="message"></textarea><br/>
           <input type="Submit"/><br/>
